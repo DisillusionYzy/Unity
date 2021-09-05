4a626f57732670d9526dda09e170061459109b15
DisillusionYzy <490023337@163.com>
2021-09-05 14:52:09 +0800

    misc

---
+++ ModTheCube/Assets/ModTheCube/Cube.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    private float rotateSpeed;
+    
+    void Start()
+    {
+        var x = Random.Range(-5.0f, 5.0f);
+        var y = Random.Range(-5.0f, 5.0f);
+        var z = Random.Range(-1.0f, 1.0f);
+        transform.position = new Vector3(x, y, z);
+
+        var scale = Random.Range(0.5f, 2.0f);
+        transform.localScale = Vector3.one * scale;
+        
+        Material material = Renderer.material;
+        var r = Random.Range(0.0f, 1.0f);
+        var g = Random.Range(0.0f, 1.0f);
+        var b = Random.Range(0.0f, 1.0f);
+        var a = Random.Range(0.0f, 1.0f);
+        material.color = new Color(r, g, b, a);
+
+        rotateSpeed = Random.Range(5.0f, 20.0f);
+    }
+    
+    void Update()
+    {
+        transform.Rotate(rotateSpeed * Time.deltaTime, 0.0f, 0.0f);
+    }
+}
