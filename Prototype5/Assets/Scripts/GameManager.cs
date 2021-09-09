b2a9f9cba9d6185e359bce03e85aabc70f1c4eb7
DisillusionYzy <490023337@163.com>
2021-09-09 18:30:21 +0800

    misc

---
+++ Prototype5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameoverText;
+    public TextMeshProUGUI hpText;
+    public Button restartButton;
+
+    private int score = 0;
+    private int hp = 3;
+    public float spawnRate = 1.0f;
+    public float ySpawnInit = -10.0f;
+    private bool isGameOver = false;
+    public bool IsGameOver
+    {
+        get
+        {
+            return isGameOver;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateScore(0);
+        SubHp(0);
+        StartCoroutine(SpawnTarget());
+
+        gameoverText.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    public void GameOver()
+    {
+        if (isGameOver)
+        {
+            return;
+        }
+        gameoverText.gameObject.SetActive(true);
+        isGameOver = true;
+        restartButton.gameObject.SetActive(true);
+
+        hp = 0;
+        SubHp(0);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("Prototype 5");
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (!isGameOver)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            var idx = Random.Range(0, targets.Count);
+            Instantiate(targets[idx], new Vector3(0, ySpawnInit, 0), targets[idx].transform.rotation);
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void SubHp(int hpToSub)
+    {
+        hp -= hpToSub;
+        if (hp <= 0)
+        {
+            hp = 0;
+            GameOver();
+        }
+        hpText.text = "Hp: " + hp;
+    }
+}
--- Prototype5/Assets/Scripts/Target.cs
+++ Prototype5/Assets/Scripts/Target.cs
@@ -4,15 +4,79 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    private GameManager gameManager;
+    private Rigidbody rb;
+    private float torqueStength = 10.0f;
+    private float upForceMin = 12.0f;
+    private float upForceMax = 15.0f;
+    private float xRange = 4.0f;
+    private float ySpawn = -1.0f;
+    public int point = 5;
+    public int subHp = 0;
+    public ParticleSystem explosionParticle;
+    public AudioClip soundClip;
+    private AudioSource soundPlay;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        soundPlay = GameObject.Find("SoundPlay").GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody>();
+        rb.position = GenSpawnPos();
+        rb.AddForce(GenForce(), ForceMode.Impulse);
+        rb.AddTorque(GenToque(), GenToque(), GenToque(), ForceMode.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Deadline")
+        {
+            Destroy(gameObject);
+
+            if (gameObject.tag == "Good" && !gameManager.IsGameOver)
+            {
+                gameManager.SubHp(subHp);
+            }
+        }
+
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameManager.IsGameOver)
+        {
+            return;
+        }
+        gameManager.UpdateScore(point);
+        Instantiate(explosionParticle, transform.position, transform.rotation);
+        soundPlay.PlayOneShot(soundClip);
+
+        if (gameObject.tag == "Bad")
+        {
+            gameManager.GameOver();
+        }
+
+        Destroy(gameObject);
+    }
+
+    private Vector3 GenForce()
+    {
+        return Vector3.up * Random.Range(upForceMin, upForceMax);
+    }
+
+    private float GenToque()
+    {
+        return Random.Range(0, torqueStength);
+    }
+
+    private Vector3 GenSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawn, 0);
     }
 }
