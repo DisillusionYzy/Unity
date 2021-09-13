b9f7fbd3a0abe2d3e904d19a797a5306d547bf39
Zhongyue Yue <490023337@163.com>
2021-09-13 22:49:21 +0800

    misc
    更新

--- SceneFlow/Assets/Scripts/MainManager.cs
+++ SceneFlow/Assets/Scripts/MainManager.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class MainManager : MonoBehaviour
 {
     public static MainManager Instance;
+    private Color teamColor;
+    public Color TeamColor
+    {
+        get => teamColor;
+        set => teamColor = value;
+    }
 
     private void Awake()
     {
+        if (Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         Instance = this;
         DontDestroyOnLoad(gameObject);
     }
