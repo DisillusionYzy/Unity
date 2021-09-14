a3f12452328b4939d8a5e7ad300ec355d21dc0c2
Zhongyue Yue <490023337@163.com>
2021-09-14 11:27:20 +0800

    misc

--- SceneFlow/Assets/Scripts/MainManager.cs
+++ SceneFlow/Assets/Scripts/MainManager.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+
+[System.Serializable]
+public class SaveData
+{
+    public Color teamColor;
+}
 
 public class MainManager : MonoBehaviour
 {
-    public static MainManager Instance;
-    private Color teamColor;
-    public Color TeamColor
-    {
-        get => teamColor;
-        set => teamColor = value;
-    }
+    public static MainManager Instance { get; private set; }
+    public Color TeamColor { get; set; }
+    private static string SavePath;
 
     private void Awake()
     {
@@ -22,5 +25,28 @@ public class MainManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+
+        SavePath = Application.persistentDataPath + "/savefile.json";
+        Debug.Log(SavePath);
+        LoadColor();
+    }
+
+    public void SaveColor()
+    {
+        var data = new SaveData();
+        data.teamColor = TeamColor;
+
+        var json = JsonUtility.ToJson(data);
+        File.WriteAllText(SavePath, json);
+    }
+
+    public void LoadColor()
+    {
+        if (File.Exists(SavePath))
+        {
+            var json = File.ReadAllText(SavePath);
+            var data = JsonUtility.FromJson<SaveData>(json);
+            TeamColor = data.teamColor;
+        }
     }
 }
